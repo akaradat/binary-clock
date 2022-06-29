7dc9d21a26bd7c3a463597654636bdaa46c10efb
Akaradat Kawmaneechot <akrd.kmnc@gmail.com>
2022-06-29 23:31:02 +0700

    feat(js): update to get current time

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ function convertTimeToBinary(timeString) {
     .map((time) => parseInt(time, 10).toString(2).padStart(4, '0'));
 }
 
+/**
+ * @return {string} time string in format HHmmss
+ */
+function getCurrentTime() {
+  const date = new Date();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}${minutes}${seconds}`;
+}
+
 function main() {
-  const currentTime = '221435'; // ['0011', '0010','0001','1000','0011','0101']
-  console.log(convertTimeToBinary(currentTime));
+  const currentTime = getCurrentTime();
+  console.log(currentTime, convertTimeToBinary(currentTime));
 }
 
 main();
