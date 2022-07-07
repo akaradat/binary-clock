edd8a83664b8c9d945d481111ce761600de73f7d
Akaradat Kawmaneechot <akrd.kmnc@gmail.com>
2022-07-07 21:02:45 +0700

    fix: rename function getCurrentTimex

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function convertTimeToBinary(timeString) {
 /**
  * @return {string} time string in format HHmmss
  */
-function getCurrentTime() {
+function getCurrentTimex() {
   const date = new Date();
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
@@ -21,7 +21,7 @@ function getCurrentTime() {
 }
 
 function main() {
-  const currentTime = getCurrentTime();
+  const currentTime = getCurrentTimex();
   console.log(currentTime, convertTimeToBinary(currentTime));
 }
 
